5fae29d60d3ec86622555d46d1a789326151d552
Taha M. A. <professor.taha@gmail.com>
2023-11-15 17:11:20 +0200

    add round results

--- Rock-Paper-Scissors-game.py
+++ Rock-Paper-Scissors-game.py
@@ -10,7 +10,7 @@ def get_user_choice():
         else:
             print("Invalid choice. Please choose Rock, Paper, Scissors, or type 'exit' to quit.")
 
-def play_game():
+def play_game(stats):
     # Generate Computer Choice
     choices = ["Rock", "Paper", "Scissors"]
     computer = random.choice(choices)
@@ -30,18 +30,30 @@ def play_game():
         (user == "Scissors" and computer == "Paper")
     ):
         result = "You win!"
+        stats['wins'] += 1
     else:
         result = "You lose!"
+        stats['losses'] += 1
 
-    # Print result
+    stats['rounds'] += 1
+
+    # Print result and stats
     print('Your choice:', user)
     print('Computer choice:', computer)
     print(result)
+    print(f"Rounds: {stats['rounds']}, You win: {stats['wins']}, You lose: {stats['losses']}")
     print()
+
+# Initialize game stats
+game_stats = {'rounds': 0, 'wins': 0, 'losses': 0}
 
 # Play the game until the user chooses to exit
 while True:
-    result = play_game()
+    result = play_game(game_stats)
     if result == 'Exit':
+        print("Game statistics:")
+        print(f"Total Rounds: {game_stats['rounds']}")
+        print(f"You Win: {game_stats['wins']}")
+        print(f"You Lose: {game_stats['losses']}")
         print("Exiting the game. Thanks for playing!")
         break
